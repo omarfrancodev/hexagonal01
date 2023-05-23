752515fe448841cb122764bed76161ca7c360dbe
Omar Franco <fofe2803@gmail.com>
2023-05-23 08:29:56 -0600

    feat: updated adding notes routes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import express from "express";
 
+import { noteRouter } from "./note/infrastructure/NotesRouter";
 import { userRouter } from "./user/infrastructure/UserRouter";
 
 const app = express();
 
 app.use("/users", userRouter);
+app.use("/notes", noteRouter);
 
 app.listen(3000, () => {
   console.log(`[Application] Server online in port 3000`);
