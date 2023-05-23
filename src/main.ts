da7451b439305596ff8b03a5b4c8913b85dc0f91
Omar Franco <fofe2803@gmail.com>
2023-05-23 10:32:37 -0600

    feat (main): add the execution of the create table script

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import express from "express";
 import { noteRouter } from "./note/infrastructure/NotesRouter";
 import { userRouter } from "./user/infrastructure/UserRouter";
 
+const db = require('./note/infrastructure/db-createTable');
+
 const app = express();
+
+db.createTable();
 
 app.use("/users", userRouter);
 app.use("/notes", noteRouter);
